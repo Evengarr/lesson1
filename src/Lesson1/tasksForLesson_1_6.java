cf341fbbe8af6fbacacef3ad2655170033544161
Evengarr <Ravasect@gmail.ru>
2020-12-06 21:49:51 +0300

    задания к первому уроку

--- src/Lesson1/tasksForLesson_1_6.java
+++ src/Lesson1/tasksForLesson_1_6.java
@@ -2,6 +2,20 @@ package Lesson1;
 
 public class tasksForLesson_1_6 {
     public static void main(String[] args) {
+        int a = 5;
+        int b = -7;
+        boolean check1 = checkingForPositivity(a);
+        System.out.println(check1);
+        boolean check2 = checkingForPositivity(b);
+        System.out.println(check2);
+    }
 
+    public static boolean checkingForPositivity(int x){
+        if (x < 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
