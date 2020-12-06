1256940599ac6d6d46d13cab04d405a1380578ba
Evengarr <Ravasect@gmail.ru>
2020-12-06 22:26:18 +0300

    задания к первому уроку

--- src/Lesson1/tasksForLesson_1_7.java
+++ src/Lesson1/tasksForLesson_1_7.java
@@ -2,13 +2,6 @@ package Lesson1;
 
 public class tasksForLesson_1_7 {
     public static void main(String[] args) {
-        String name1 = "Max";
-        String name2 = "Max 'Evengarr' Mednov";
-        sayHello(name1);
-        sayHello(name2);
-    }
 
-    public static void sayHello(String name){
-        System.out.println("Hi, my name is " + name);
     }
 }
