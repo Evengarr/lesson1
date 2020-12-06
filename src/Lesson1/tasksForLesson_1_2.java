a43cd33cf90fcc42a20bc60590057e6370a33837
Evengarr <Ravasect@gmail.ru>
2020-12-06 21:24:50 +0300

    задания к первому уроку

--- src/Lesson1/tasksForLesson_1_2.java
+++ src/Lesson1/tasksForLesson_1_2.java
@@ -2,15 +2,7 @@ package Lesson1;
 
 public class tasksForLesson_1_2 {
     public static void main(String[] args) {
-        byte a = 12;
-        short b = 253;
-        int c = 56386;
-        long d = 454686556;
-        float e = 12.26f;
-        double f = 123.659;
-        char g = '9';
-        boolean h = false;
-        String i = "Hello, World!";
+
     }
 
 }
--- src/Lesson1/tasksForLesson_1_3.java
+++ src/Lesson1/tasksForLesson_1_3.java
@@ -2,16 +2,6 @@ package Lesson1;
 
 public class tasksForLesson_1_3 {
     public static void main(String[]args){
-        float x = 12.7f;
-        float y = 22.16f;
-        float z = 1.65f;
-        float w = 17.77f;
-        float result = calculation(x, y, z, w);
-        System.out.println(result);
-    }
 
-    static float calculation(float a, float b, float c, float d) {
-        float r = a * (b + (c / d));
-        return r;
     }
 }
