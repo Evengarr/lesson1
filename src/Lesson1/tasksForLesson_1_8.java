bb07763b5a3c7a8e172376d640c0eb286ef2d085
Evengarr <Ravasect@gmail.ru>
2020-12-06 22:29:31 +0300

    задания к первому уроку

--- src/Lesson1/tasksForLesson_1_8.java
+++ src/Lesson1/tasksForLesson_1_8.java
@@ -2,6 +2,15 @@ package Lesson1;
 
 public class tasksForLesson_1_8 {
     public static void main(String[] args) {
-
+        int year = 2020;
+        theDefinitionOfTheYear(year);
+    }
+    public static void theDefinitionOfTheYear(int x) {
+        if(x % 4 == 0 && x % 100 !=0 || x % 400 == 0){
+            System.out.println(x + " leap year");
+        }
+        else {
+            System.out.println(x + " not a leap year");
+        }
     }
 }
